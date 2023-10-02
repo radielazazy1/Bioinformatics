c38cd6e7e613fab8d5e1a81faba900d24f2198cb
radielazazy1 <145633929+radielazazy1@users.noreply.github.com>
2023-10-02 12:38:48 -0400

    Update one.py

--- one.py
+++ one.py
@@ -1,3 +1,16 @@
 from itertools import product
 
-d
+def debruijn(x):
+  L = len(x[0])
+
+  #all possible kmers
+  kmers = set()
+  for y in x:
+    for i in range(L-1)
+      kmers = x[i:i+2]
+      kmers.add(newkmer)
+
+  allkmers = ''.join(x) for x in product(kmers, repeat=L)
+
+  
+    
